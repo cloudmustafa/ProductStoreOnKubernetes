ebcc80ad5905246aa2fb2ca33f55cf79efa096a6
Akhterul Mustafa <amustafa2020@gmail.com>
2019-11-27 08:37:09 -0600

    first commit

---
+++ create-aks-cluster.sh
@@ -0,0 +1,24 @@
+# connect to Azure via Azure CLI
+az login
+
+# create variables
+# AksName="aks-k8s-extia"
+# AksRG="aks-k8s-extia"
+
+# Create resource group
+az group create --location westeurope --subscription "Visual Studio Enterprise"  --name aks-k8s-rg 
+
+# create AKS cluster
+az aks create --generate-ssh-keys --subscription "Visual Studio Enterprise" --node-count 3 --resource-group aks-k8s-rg --name aks-k8s
+
+# connect to cluster
+az aks get-credentials \
+   --resource-group aks-k8s-rg \
+   --name aks-k8s \
+   #--subscription "Microsoft Azure Sponsorship"
+
+# get access to Dashboard
+kubectl create clusterrolebinding kubernetes-dashboard --clusterrole=cluster-admin --serviceaccount=kube-system:kubernetes-dashboard
+
+# Open Dashboard
+az aks browse --resource-group aks-k8s-rg --name aks-k8 --subscription "Visual Studio Enterprise" 
